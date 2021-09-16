3b66a2d6849a5fff14d8b31885329db2314bbeb0
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2021-09-16 16:37:33 +0200

    ajout des fichiers DiffusionHypercube et DiffusionAnneau

--- DiffusionHypercube.py
+++ DiffusionHypercube.py
@@ -1,8 +1,8 @@
 from mpi4py import MPI
 
-#mpiexec -n 4 py HelloWorld.py
-
 comm = MPI.COMM_WORLD
 me = comm.Get_rank()
 size = comm.Get_size()
 print("Hi from <"+str(me)+">")
+if me == 0:
+    buf = ["coucou"]
