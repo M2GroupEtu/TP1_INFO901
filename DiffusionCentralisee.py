30f208fde89fdca6e105c615f7c17dea2f0d61b3
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2021-09-17 08:54:11 +0200

    diffusion centralisée fonctionnelle

--- DiffusionCentralisee.py
+++ DiffusionCentralisee.py
@@ -1,15 +1,21 @@
 from mpi4py import MPI
 import sys
 
+if len(sys.argv) > 1 :
+    sender = int(sys.argv[1])
+
 comm = MPI.COMM_WORLD
 me = comm.Get_rank()
 size = comm.Get_size()
+
 print("Hi from <"+str(me)+">")
-if me == 0:
+
+if me == sender:
     buf = ["coucou"]
     print("I'm <"+str(me)+">: send " + buf[0])
-    for i in range(1, size):
-	    comm.send(buf, dest=i, tag=99)
+    for i in range(0, size):
+        if(i != sender):
+	        comm.send(buf, dest=i, tag=99)
 else:
-    buf = comm.recv(source=0, tag=99)
+    buf = comm.recv(source=sender, tag=99)
     print("I'm <"+str(me)+">: receive " + buf[0])
