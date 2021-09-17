8461ee69b02b5973cebb5cc494895a00550bf831
Armadias <follietmartin@gmail.com>
2021-09-17 10:41:38 +0200

    algo 2 sens marche pas

--- DiffusionAnneau.py
+++ DiffusionAnneau.py
@@ -1,10 +1,12 @@
 from mpi4py import MPI
 import sys
 
-if (len(sys.argv) > 1) :
+if (len(sys.argv) > 2) :
     _from = int(sys.argv[1])
+    sens = int(sys.argv[2])
 else :
     _from = 0
+    sens = 1
 
 comm = MPI.COMM_WORLD
 me = comm.Get_rank()
@@ -16,18 +18,54 @@ if _from >= size:
     print("SOLUTION : _FROM MUST BE LESS THAN : " + str(size - 1))
     exit()
 
-last = (_from + size - 1) % size
 prev = (me - 1 + size) % size
 next = (me + 1) % size
 
+print("NEXT : " + str(prev))
+print("PREV : " + str(next))
 
-if me == _from:
-    buf = ["coucou"]
-    comm.send(buf, dest= next, tag=99)
-elif me != last :
-    buf = comm.recv(source = prev , tag=99)
-    comm.send(buf, dest= next, tag=99)
+def anneau_1_sens() :
+    last = (_from + size - 1) % size
+
+    if me == _from:
+        buf = ["coucou"]
+        comm.send(buf, dest= next, tag=99)
+
+    elif me != last :
+        buf = comm.recv(source = prev , tag=99)
+        comm.send(buf, dest= next, tag=99)
+
+    else :
+        buf = comm.recv(source= prev , tag=99)
+
+    print("I'm <"+str(me)+">: receive " + buf[0])
+
+def anneau_2_sens():
+    last_from_0 = round((size/2) + 0.1)
+    true_last = (last_from_0 + _from) % size
+
+    if me == _from:
+        buf = ["coucou"]
+        comm.send(buf, dest= next, tag=99)
+        comm.send(buf, dest= prev, tag=99)
+
+    elif me == true_last:
+        buf = comm.recv(source= prev , tag=99)
+        comm.recv(source= next , tag=99)
+
+    elif me < (_from + size)/2 :
+        buf = comm.recv(source= prev , tag=99)
+        comm.send(buf, dest= next, tag=99)
+
+    elif me > (_from + size)/2 :
+        buf = comm.recv(source= next , tag=99)
+        comm.send(buf, dest= prev, tag=99)
+    
+    print("I'm <"+str(me)+">: receive " + buf[0])
+
+
+#APPEL DES FONCTIONS
+if sens == 1 :
+    anneau_1_sens()
 else :
-    buf = comm.recv(source= prev , tag=99)
-
-print("I'm <"+str(me)+">: receive " + buf[0])
+    anneau_2_sens()
--- HelloWorld.py
+++
@@ -1,9 +0,0 @@
-from mpi4py import MPI
-
-#mpiexec -n 4 py HelloWorld.py
-#test
-
-comm = MPI.COMM_WORLD
-me = comm.Get_rank()
-size = comm.Get_size()
-print("Hi from <"+str(me)+">")
